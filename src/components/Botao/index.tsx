965cd5b5032595a68c4f3b72ce64dc195c21d3b7
brunolazzara <lazzara.bruno@gmail.com>
2023-08-08 22:16:58 -0300

    Refatora class components para function components

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import style from './Botao.module.scss';
 
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+    children?: React.ReactNode
+}
+
+function Botao({ onClick, type, children }: Props) {
+    return (
+        <button type={type} className={style.botao} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
 // Class components estão se tornando deprecated,
 // e usa-se function components no seu lugar (ver component Lista)
-class Botao extends React.Component<any, {
-    type: "button" | "submit" | "reset" | undefined
-}> {
-    render() {
-        const { type = "button", onClick } = this.props;
-        return (
-            <button type={type} className={style.botao} onClick={onClick}>
-                {this.props.children}
-            </button>
-        )
-    }
-}
+
+// class Botao1 extends React.Component<{
+//     type?: "button" | "submit" | "reset" | undefined,
+//     onClick?: () => void
+// }> {
+//     render() {
+//         const { type = "button", onClick } = this.props;
+//         return (
+//             <button type={type} className={style.botao} onClick={onClick}>
+//                 {this.props.children}
+//             </button>
+//         )
+//     }
+// }
 
 export default Botao;
